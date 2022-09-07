0cff1a680ed7614b4fa5f4e31d0a2b23ea2d5935
Your Name <you@example.com>
2022-09-07 17:17:01 +0900

    キータのコードをコピペしてどんな動きをするか見てみようとしたが、エラーがでてしまった メールのデータを取得できていないのか？

--- src/auth/login/index.php
+++ src/auth/login/index.php
@@ -1,3 +1,35 @@
+<?php
+session_start();
+$mail = $_POST['mail'];
+$dsn = "mysql:host=localhost; dbname=xxx; charset=utf8";
+$username = "xxx";
+$password = "xxx";
+try {
+    $dbh = new PDO($dsn, $username, $password);
+} catch (PDOException $e) {
+    $msg = $e->getMessage();
+}
+
+$sql = "SELECT * FROM users WHERE mail = :mail";
+$stmt->bindValue(':mail', $mail);
+$stmt->execute();
+$member = $stmt->fetch();
+//指定したハッシュがパスワードにマッチしているかチェック
+if (password_verify($_POST['pass'], $member['pass'])) {
+    //DBのユーザー情報をセッションに保存
+    $_SESSION['id'] = $member['id'];
+    $_SESSION['name'] = $member['name'];
+    $msg = 'ログインしました。';
+    $link = '<a href="index.php">ホーム</a>';
+} else {
+    $msg = 'メールアドレスもしくはパスワードが間違っています。';
+    $link = '<a href="login.php">戻る</a>';
+}
+?>
+
+<h1><?php echo $msg; ?></h1>
+<?php echo $link; ?>
+
 <!DOCTYPE html>
 <html lang="ja">
 
@@ -21,7 +53,7 @@
   <main class="bg-gray-100 h-screen">
     <div class="w-full mx-auto py-10 px-5">
       <h2 class="text-md font-bold mb-5">ログイン</h2>
-      <form action="/" method="POST">
+      <form action="index.php" method="POST">
         <input type="email" placeholder="メールアドレス" class="w-full p-4 text-sm mb-3">
         <input type="password" placeholder="パスワード" class="w-full p-4 text-sm mb-3">
         <label class="inline-block mb-6">
