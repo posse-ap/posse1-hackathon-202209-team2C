90e22393de1dcd1217e698cdd74a857a23dd5c1e
{ayaka-fujiyoshi} <ayaka1712pome@gmail.com>
2022-09-08 16:50:31 +0900

    テーブル結合の方法を変えてみたsql分を追記

--- src/unanswered.php
+++ src/unanswered.php
@@ -38,15 +38,26 @@ $sql = 'SELECT
           left outer join users 
           on event_attendance.user_id = users.id 
           WHERE DATE_FORMAT(start_at, "%Y-%m-%d %H:%i:%s") >= DATE_FORMAT(now(), "%Y-%m-%d %H:%i:%s") 
-             and event_attendance.attendance = true
+             and event_attendance.attendance Is Null
+          order by events.start_at; ';
+$sql = 'SELECT
+          events.name AS e_name, DATE_FORMAT(events.start_at, "%Y/%m/%d") AS date, users.user_name, event_attendance.attendance AS attend 
+          from users 
+          left outer join event_attendance 
+          on users.id = event_attendance.user_id
+          right outer join events
+          on event_attendance.event_id = events.id
+          WHERE DATE_FORMAT(start_at, "%Y-%m-%d %H:%i:%s") >= DATE_FORMAT(now(), "%Y-%m-%d %H:%i:%s") 
           order by events.start_at; ';
 
+//参考↓/------------------------------------------------------
 $sql = 'SELECT 
           events.name AS e_name, DATE_FORMAT(events.start_at, "%Y/%m/%d") AS date, users.user_name
           FROM Table1 AS a 
           LEFT JOIN (select user_id From Table1 where item_id = 2)  AS b
           ON a.user_id = b.user_id
           WHERE a.item_id=1 AND b.user_id Is Null;';
+//------------------------------------------------------
 
 $stmt = $db->query($sql);
 $events_table = $stmt->fetchAll();
